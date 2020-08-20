9065c21df0f029949ad7f4c40b7580db3dde71c3
Zenahr Barzani <zenmatica@gmail.com>
2020-08-20 23:38:48 +0200

    finished date formatting output

--- site-builder/builder.py
+++ site-builder/builder.py
@@ -1,16 +1,16 @@
 from jinja2 import Template
 import json
 import pprint
-import datetime
+from datetime import datetime as dt
 
 def date_formatter(entry):
             input = entry['created_at'] # "Wed Aug 05 04:21:47 UTC 2020"
-            output = datetime.datetime.strptime(input, '%a %b %d %H:%M:%S %Z %Y')
-            # print(output)
-            today = datetime.datetime.today()
-            difference = today - output
-            # print(today)
-            entry['created_at'] = difference
+            today = dt.today()
+            difference = str(today - dt.strptime(input, '%a %b %d %H:%M:%S %Z %Y'))
+            if("day" in difference):
+                entry['created_at'] =  difference.split(',')[0] + " ago"
+            else:
+                entry['created_at'] = "today"
 
 def formatter(data):
     for entry in data:
@@ -32,21 +32,3 @@ with open("scraped-jobs.json", "r", encoding="utf8") as f:
 # Write generated html
 with open('../public/index.html', 'w', encoding="utf-8") as output_file:
     output_file.write(template.render(jobs=data))
-
-
-# for job in data:
-#     print("ID:", job["id"])
-#     print("Type:", job["type"])
-#     print("URL:", job["url"])
-#     print("Posted:", job["created_at"])
-#     print("Company:", job["company"])
-#     print("Company URL:", job["company_url"])
-#     print("Location:", job["location"])
-#     print("Job Title:", job["title"])
-#     print("Job Description:", job["description"])
-#     # print("How To Apply:", job["how_to_apply"])
-#     # print("Company Logo:", job["company_logo"])
-#     print()
-#     print("----------------------------------------------------------------------")
-#     print("----------------------------------------------------------------------")
-#     print()
