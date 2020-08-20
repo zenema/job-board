a97370a4bcd84fdfb6b6c6971e46aab778b9ffb5
Zenahr Barzani <zenmatica@gmail.com>
2020-08-20 22:43:02 +0200

    date formatter built

--- site-builder/builder.py
+++ site-builder/builder.py
@@ -1,5 +1,20 @@
 from jinja2 import Template
 import json
+import pprint
+import datetime
+
+def date_formatter(entry):
+            input = entry['created_at'] # "Wed Aug 05 04:21:47 UTC 2020"
+            output = datetime.datetime.strptime(input, '%a %b %d %H:%M:%S %Z %Y')
+            # print(output)
+            today = datetime.datetime.today()
+            difference = today - output
+            # print(today)
+            entry['created_at'] = difference
+
+def formatter(data):
+    for entry in data:
+        date_formatter(entry)
 
 # Get template
 with open('template.j2') as file_:
@@ -8,12 +23,16 @@ with open('template.j2') as file_:
 # Stream jobs into script
 with open("scraped-jobs.json", "r", encoding="utf8") as f:
     data = json.load(f)
+    formatter(data)
     f.close()
     # print(data)
+    # pprint.pprint(data, indent=2)
+
 
 # Write generated html
 with open('../public/index.html', 'w', encoding="utf-8") as output_file:
     output_file.write(template.render(jobs=data))
+
 
 # for job in data:
 #     print("ID:", job["id"])
